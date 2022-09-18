ce5726f462df43773449a2bc3aba0cc4a1fb6314
Jelena Cuka <cuka.jelena@gmail.com>
2022-09-18 15:24:48 +0200

    split aolution to Core and Data projects, updated using statements, package updates

--- HotelListing.API/Exceptions/NotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HotelListing.API.Exceptions
-{
-    public class NotFoundException: ApplicationException
-    {
-        public NotFoundException(string name, object key):base($"{name} and {key} was not found.")
-        {
-        }
-    }
-}

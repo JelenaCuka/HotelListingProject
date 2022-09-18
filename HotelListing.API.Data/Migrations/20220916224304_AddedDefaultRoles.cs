1c11105f214090edcf4f509cbbd60848ec18344b
Jelena Cuka <cuka.jelena@gmail.com>
2022-09-18 22:49:25 +0200

    solution split to projects, added Data and Core projects, updated namespaces, usings and packages.

---
+++ HotelListing.API.Data/Migrations/20220916224304_AddedDefaultRoles.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Data.Migrations
+{
+    public partial class AddedDefaultRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "2a0f2bf5-a33b-4df5-901a-a1ae0129758c", "05c73820-9939-42c3-b93b-e7593b51b547", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "9793faa6-09e8-49ce-9784-d4c40ce5da83", "d805dfe6-5be1-46bc-af52-245468d019fa", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2a0f2bf5-a33b-4df5-901a-a1ae0129758c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9793faa6-09e8-49ce-9784-d4c40ce5da83");
+
+        }
+    }
+}
